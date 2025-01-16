e3737eb811fa2f952924de52bb5fde1f3d92742c
hemantmm <hemant29mehta@gmail.com>
2025-01-16 22:40:34 +0530

    Initial Commit

---
+++ musicApp/musicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  musicAppApp.swift
+//  musicApp
+//
+//  Created by Hemant Mehta on 16/01/25.
+//
+
+import SwiftUI
+
+@main
+struct musicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ musicAppTests/musicAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  musicAppTests.swift
+//  musicAppTests
+//
+//  Created by Hemant Mehta on 16/01/25.
+//
+
+import Testing
+
+struct musicAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
